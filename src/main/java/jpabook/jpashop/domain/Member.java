074c0092c404773bf90edeec4bd404b3c2afddb3
JJ0ng <2000jiyong@gmail.com>
2023-03-16 22:26:26 +0900

    엔티티 개발1

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,20 +1,24 @@
 package jpabook.jpashop.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter @Setter
 public class Member {
     @Id @GeneratedValue
-
+    @Column(name = "member_id")
     private Long id;
 
     private String name;
 
+    @Embedded
     private Address address;
 
+    @OneToMany
     private List<Order> orders = new ArrayList<>();
 }
