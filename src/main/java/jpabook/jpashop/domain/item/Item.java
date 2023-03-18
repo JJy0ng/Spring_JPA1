87b9dfbac5046358d39afbf6192aa3638c7c566e
JJ0ng <2000jiyong@gmail.com>
2023-03-18 19:47:08 +0900

    엔티티 개발3

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,9 +1,12 @@
 package jpabook.jpashop.domain.item;
 
+import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 상속 관계의 전략을 지정해야함(부모클래스)
@@ -18,5 +21,8 @@ public abstract class Item {
     private String name;
     private int price;
     private  int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> items = new ArrayList<>();
 
 }
