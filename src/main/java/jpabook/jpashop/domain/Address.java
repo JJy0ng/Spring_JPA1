1c5ca219388614381dea6dcc5ec1efc7228069ef
JJ0ng <2000jiyong@gmail.com>
2023-03-19 18:50:43 +0900

    엔티티 개발4

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,14 +1,24 @@
 package jpabook.jpashop.domain;
 
 import lombok.Getter;
-import lombok.Setter;
+
 
 import javax.persistence.Embeddable;
 
 @Embeddable //내장값
-@Getter @Setter
+@Getter
 public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    //함부로 생성을 막음
+    protected Address() {
+    }
+    //Setter를 제거해서 생성자에서 값을 모두 초기화해서 변경 불가능한 클래스를 만듬
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
