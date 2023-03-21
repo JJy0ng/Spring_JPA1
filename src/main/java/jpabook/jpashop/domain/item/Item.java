cc2f1732f2aaf83dcf9ed4d54f578ac28d75bace
JJ0ng <2000jiyong@gmail.com>
2023-03-21 19:46:40 +0900

    상품 엔티티 개발(비지니스 로직 추가)

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +12,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 상속 관계의 전략을 지정해야함(부모클래스)
 @DiscriminatorColumn(name = "dtype")
-@Setter @Getter
+@Getter
 public abstract class Item {
     @Id
     @GeneratedValue
@@ -25,4 +26,22 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    //==비지니스 로직==// , Setter를 사용하지 않고 메서드를 통해서 하는게 좋다
+    /**
+     *  stock(재고) 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removerStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
